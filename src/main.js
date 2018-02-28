bd985a1d7039f95f19413aad71a728c20ce0ed38
qunwang <niuyiwangqun@sohu.com>
2018-03-01 00:12:14 +0800

    change

--- src/main.js
+++ src/main.js
@@ -1,3 +1,3 @@
 import a from 'aaa';
-
+import './style.css'
 console.log('bbb')
--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,14 @@ module.exports = {
     output: {
         path: __dirname + "/dist", //打包后的文件存放的地方
         filename: "bundle.js" //打包后输出文件的文件名
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }]
     }
 }
