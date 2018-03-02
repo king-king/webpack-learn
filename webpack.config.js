fc871777c667be1953b43dca6573d07dc6ea8c33
wangqun6 <wangqun28>
2018-03-02 15:51:35 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,18 @@ module.exports = {
     output: {
         path: __dirname + "/dist", //打包后的文件存放的地方
         filename: "bundle.js" //打包后输出文件的文件名
-    }
+    },
+    //插件数组
+    plugins: [
+        //初始化插件,传递模板参数
+        new htmlWebpackPlugin({
+            //模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
+            template: '/view/index.html',
+            //自动生成HTML文件的名字
+            filename: 'index.html',
+            //引入打包后的js的script标签所在的位置,false表示不自动引入打包后的js
+            //可以向模板传递参数，然后应用于自动生成的html,(模板需要获取参数)
+            title: '我来自参数'
+        })
+    ]
 }
