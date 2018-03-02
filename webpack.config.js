59c34ba7911d97e8bfd17ef1b47815223eb5d646
wangqun6 <wangqun28>
2018-03-02 17:14:02 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,12 @@ module.exports = {
     entry: __dirname + "/src/main.js", //已多次提及的唯一入口文件
     output: {
         path: __dirname + "/dist", //打包后的文件存放的地方
-        filename: "bundle.js" //打包后输出文件的文件名
+        filename: "[name]_[hash:8].js" //打包后输出文件的文件名
     },
     //插件数组
     plugins: [
         new ExtractTextPlugin({
-            filename: "[name].css"
+            filename: "[name]_[contenthash:8].css"
         }),
         //初始化插件,传递模板参数
         new htmlWebpackPlugin({
