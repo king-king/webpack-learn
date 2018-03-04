bc35b816c6a71843c4c21c5ad0fc639816f99aa5
qunwang <niuyiwangqun@sohu.com>
2018-03-04 22:37:20 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     output: {
         path: __dirname + "/dist", //打包后的文件存放的地方
         // js输出路径,可以使用文件夹结构
-        filename: "static/js/[name]_[hash:8].js" //打包后输出文件的文件名
+        filename: "static/js/[name]_[chunkhash:8].js" //打包后输出文件的文件名
     },
     //插件数组
     plugins: [
@@ -30,10 +30,14 @@ module.exports = {
             template: 'view/index.html',
             //自动生成HTML文件的名字,可以嵌套文件夹
             filename: 'view/index.html',
-            //引入打包后的js的script标签所在的位置,false表示不自动引入打包后的js
-            //可以向模板传递参数，然后应用于自动生成的html,(模板需要获取参数)
-            title: '我来自参数',
             chunks: ['index']
+        }),
+        new htmlWebpackPlugin({
+            //模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
+            template: 'view/login.html',
+            //自动生成HTML文件的名字,可以嵌套文件夹
+            filename: 'view/login.html',
+            chunks: ['login']
         })
     ],
     module: {
