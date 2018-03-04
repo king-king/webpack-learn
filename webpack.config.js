41efb3d8f2f784bf525d301f6988dd4529c9dd0c
qunwang <niuyiwangqun@sohu.com>
2018-03-04 20:52:42 +0800

    change

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-var htmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
 
 module.exports = {
     // 入口文件
@@ -13,6 +15,11 @@ module.exports = {
     },
     //插件数组
     plugins: [
+        new CleanWebpackPlugin('dist/*', {
+            root: __dirname,
+            verbose: true,
+            dry: false
+        }),
         new ExtractTextPlugin({
             filename: "static/[name]_[contenthash:8].css"
         }),
@@ -20,11 +27,12 @@ module.exports = {
         new htmlWebpackPlugin({
             //模板为同级目录下的index.html，为何不用写路径，是因为默认上下文问webpack.config.js所在的文件夹
             template: 'view/index.html',
-            //自动生成HTML文件的名字
+            //自动生成HTML文件的名字,可以嵌套文件夹
             filename: 'index.html',
             //引入打包后的js的script标签所在的位置,false表示不自动引入打包后的js
             //可以向模板传递参数，然后应用于自动生成的html,(模板需要获取参数)
-            title: '我来自参数'
+            title: '我来自参数',
+            chunks: ['index']
         })
     ],
     module: {
