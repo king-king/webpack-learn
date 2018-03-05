ef3e59b1ebf695f0c669b90a58335795b758cdbb
wangqun6 <wangqun28>
2018-03-05 14:13:00 +0800

    change

--- dist/static/js/index_168ba0db.js
+++ dist/static/js/index_168ba0db.js
@@ -67,40 +67,40 @@
 /* 0 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_style_scss__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_style_scss__);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__component_com_js__ = __webpack_require__(2);
+    "use strict";
+    Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_style_scss__ = __webpack_require__(1);
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_style_scss__);
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__component_com_js__ = __webpack_require__(2);
 
 
 
 
-/***/ }),
-/* 1 */
-/***/ (function(module, exports) {
+    /***/ }),
+    /* 1 */
+    /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+    // removed by extract-text-webpack-plugin
 
-/***/ }),
-/* 2 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+    /***/ }),
+    /* 2 */
+    /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_com_css__ = __webpack_require__(3);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_com_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_com_css__);
+    "use strict";
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_com_css__ = __webpack_require__(3);
+    /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__style_com_css___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__style_com_css__);
 
 
-var div = document.createElement('div');
-div.classList.add('com');
-div.innerHTML = 'com';
-document.querySelector('#root').appendChild(div);
+    var div = document.createElement('div');
+    div.classList.add('com');
+    div.innerHTML = 'com';
+    document.querySelector('#root').appendChild(div);
 
-/***/ }),
-/* 3 */
-/***/ (function(module, exports) {
+    /***/ }),
+    /* 3 */
+    /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+    // removed by extract-text-webpack-plugin
 
-/***/ })
-/******/ ]);
+    /***/ })
+    /******/ ]);
